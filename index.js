6c9fddcf8f15ae7d468520e3fe51148c4b885faf
Ben Rowe <brow495@gmail.com>
2020-11-03 11:35:05 -0700

    Add Fauci favicon.

--- index.js
+++ index.js
@@ -21,8 +21,15 @@ app.post('/chatbot', (request, response, next) => {
     .catch((error) => response.send({ 'ERROR': error}))
 })
 
-app.get('/', (request, response) => {
-    return 'hi'
+app.get('/hi', (request, response) => {
+    return (
+        <div>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="manifest" href="/site.webmanifest"/>
+        </div>
+    )
 })
 
 const PORT = process.env.PORT || 5000
