619424e7b791f3d3e676980a8f54089198f4a4e7
Ben Rowe <brow495@gmail.com>
2020-11-03 11:19:01 -0700

    Add get function to test server.

--- index.js
+++ index.js
@@ -21,5 +21,9 @@ app.post('/chatbot', (request, response, next) => {
     .catch((error) => response.send({ 'ERROR': error}))
 })
 
+app.get('/', (request, response) => {
+    return 'hi'
+})
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, '0.0.0.0')
