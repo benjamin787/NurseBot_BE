7de3e07b48daf62ac37678682456cf4c98786a08
Ben Rowe <brow495@gmail.com>
2020-11-02 22:34:45 -0700

    Add google credentials.

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const PORT = process.env.DATABASE_URL || 4000
 const bodyParser = require('body-parser')
 app.use(bodyParser)
 
+const cors = require('cors')
+
+const serviceAccount = require('./service-account.json')
+
 app.get('/', (req, res) => {
     return 'hi'
 })
