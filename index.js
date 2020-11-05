d6ff6c8d8dac0e9a390974ad974d161335d510c2
Ben Rowe <brow495@gmail.com>
2020-11-05 13:48:29 -0700

    Connect.

--- index.js
+++ index.js
@@ -12,23 +12,35 @@ app.use(bodyParser.json())
 
 const { WebhookClient } = require('dialogflow-fulfillment')
 
-//might have to pass express.json() in
+const dialogflow = require('@google-cloud/dialogflow')
+
+const project_id = JSON.parse(process.env.PROJECT_ID)
+
+const config = {
+    credentials: {
+        private_key: JSON.parse(process.env.PRIVATE_KEY),
+        client_email: JSON.parse(process.env.CLIENT_EMAIL)
+    },
+    project_id: project_id
+}
+
+const session_id = `${Math.floor(Math.random() * 1000) + 1}`
+
+const sessionClient = new dialogflow.SessionsClient(config)
+const sessionPath = sessionClient.projectAgentSessionPath(project_id, session_id)
+
 app.post('/chatbot', (req, res) => {
     const agent = new WebhookClient({req, res})
 
-    function test(agent) {
+    function fx(agent) {
         agent.add('you got this!')
     }
     let intentMap = new Map()
-    intentMap.set('Default Welcome Intent', test)
+    intentMap.set('Default Welcome Intent', fx)
     agent.handleRequest(intentMap)
+    res.status(200).end()
 })
 
-
-
-
-const connectToDF = require('./chatbot.js')
-const { request } = require('express')
 
 // app.post('/chatbot', (request, response) => {
 //     connectToDF(request)
@@ -36,7 +48,7 @@ const { request } = require('express')
 //         .catch((error) => response.send({ 'ERROR': error}))
 // })
 
-app.get('/', (request, response) => response.json({message: "Hello"}))
+// app.get('/', (request, response) => response.json({message: "Hello"}))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, '0.0.0.0')
