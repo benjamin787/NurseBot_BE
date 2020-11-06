da576ef12f57c9e472cb95786fa8256cb553a810
Ben Rowe <brow495@gmail.com>
2020-11-06 14:08:01 -0700

    UD.4

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const dialogflow = require('@google-cloud/dialogflow');
+
 const express = require('express')
 const app = express()
 
@@ -13,21 +15,86 @@ app.use(bodyParser.json())
 
 app.options('/chatbot', cors())
 
-const { WebhookClient } = require('dialogflow-fulfillment')
+// Instantiates a session client
+const sessionClient = new dialogflow.SessionsClient();
 
-app.post('/chatbot', (request, response) => {
-    response.headers = {"Access-Control-Allow-Origin": "https://covid-nurse-bot.web.app"}
-    dialogflowFulfillment(request, response)
-})
+const uuid = require('uuid')
 
-const dialogflowFulfillment = (request, response) => {
-    const agent = new WebhookClient({request, response})
+const projectId = process.env.PROJECT_ID
 
-    function sayHello(agent) {
-        agent.add('yay')
-    }
-
-    let intentMap = new Map()
-    intentMap.set('Default Welcome Intent', sayHello)
-    agent.handleRequest(intentMap)
+const options = {
+    credentials: {
+        private_key: process.env.PRIVATE_KEY,
+        client_email: process.env.CLIENT_EMAIL
+        // private_key: JSON.parse(process.env.PRIVATE_KEY),
+        // client_email: JSON.parse(process.env.CLIENT_EMAIL)
+    },
+    projectId: projectId
 }
+
+// const session_id = `${Math.floor(Math.random() * 1000) + 1}`
+const sessionId = uuid.v4()
+
+
+
+
+async function detectIntent(
+  projectId,
+  sessionId,
+  query,
+  contexts,
+  languageCode
+) {
+  // The path to identify the agent that owns the created intent.
+  const sessionPath = sessionClient.projectAgentSessionPath(
+    projectId,
+    sessionId
+  );
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: query,
+        languageCode: languageCode,
+      },
+    },
+  };
+
+  if (contexts && contexts.length > 0) {
+    request.queryParams = {
+      contexts: contexts,
+    };
+  }
+
+  const responses = await sessionClient.detectIntent(request);
+  return responses[0];
+}
+
+async function executeQueries(projectId, sessionId, queries, languageCode) {
+  // Keeping the context across queries let's us simulate an ongoing conversation with the bot
+  let context;
+  let intentResponse;
+  for (const query of queries) {
+    try {
+      console.log(`Sending Query: ${query}`);
+      intentResponse = await detectIntent(
+        projectId,
+        sessionId,
+        query,
+        context,
+        languageCode
+      );
+      console.log('Detected intent');
+      console.log(
+        `Fulfillment Text: ${intentResponse.queryResult.fulfillmentText}`
+      );
+      // Use the context from this response for next queries
+      context = intentResponse.queryResult.outputContexts;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+executeQueries(projectId, sessionId, queries, languageCode);
