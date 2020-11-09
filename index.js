f5ebc47ae70e95a2ede43c7619f4c4adf6028e32
Ben Rowe <brow495@gmail.com>
2020-11-09 11:26:08 -0700

    Identify context. Clean up.

--- index.js
+++ index.js
@@ -28,8 +28,6 @@ const options = {
     credentials: {
         "private_key": JSON.parse(process.env.PRIVATE_KEY),
         "client_email": process.env.CLIENT_EMAIL
-        // private_key: JSON.parse(process.env.PRIVATE_KEY),
-        // client_email: JSON.parse(process.env.CLIENT_EMAIL)
     }
 }
 
@@ -43,7 +41,7 @@ app.post('/chatbot', async (request, response) => {
     const dialogClient = new dialogflow.SessionsClient(options);
 
     const sessionPath = dialogClient.projectAgentSessionPath(projectId, sessionId);
-    // const sessionPath = dialogClient.sessionPath(projectId, sessionId);
+
     console.log('req body', request.body)
     const botRequest = {
         session: sessionPath,
@@ -54,53 +52,18 @@ app.post('/chatbot', async (request, response) => {
             }
         }
     }
-
+    if (contexts && contexts.length > 0) {
+        request.queryParams = {contexts: contexts};
+    }
     try {
         let botResult = await dialogClient.detectIntent(botRequest)
         console.log('botresult',botResult)
         response.send(botResult[0])
     } catch(error) {
         console.log(error)
+        response.send({message: 'blahblah'})
     }
-
 })
-// app.post('/chatbot', (request, response) => {
-//     console.log('projectId', projectId)
-//     console.log('sessionId', sessionId)
-//     response.json(conversationTurn(sessionId, request))
-// })
-
-
-// const conversationTurn = async (sessionId, data) => {
-//     const dialogClient = new dialogflow.SessionsClient(options);
-
-//     const sessionPath = dialogClient.projectAgentSessionPath(projectId, sessionId);
-//     // const sessionPath = dialogClient.sessionPath(projectId, sessionId);
-//     console.log('data body', data.body)
-//     const botRequest = {
-//         session: sessionPath,
-//         queryInput: {
-//             text: {
-//                 text: data.body.message,
-//                 languageCode: "en-US"
-//             }
-//         }
-//     }
-
-//     let botResult = await dialogClient.detectIntent(botRequest)
-//         .then((botResult) => {
-//             console.log('botresult', botResult)
-//             const result = botResult[0].queryResult
-//             if (result.intent) {
-//                 console.log('intent', result.intent)
-//             } else {
-//                 console.log('no intent')
-//             }
-//         }).catch(error => console.log(error))
-//     response.send({ do: "text query" })
-    
-// }
-
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, '0.0.0.0')
