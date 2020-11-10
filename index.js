bae4c5ec9aab9c881b4f1583db5099bb226e8acb
Ben Rowe <brow495@gmail.com>
2020-11-10 08:45:43 -0700

    Add matchIntent and findTest functions.

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.options('/chatbot', cors())
 
 const uuid = require('uuid');
 const { response } = require('express');
+const axios = require('axios');
 const sessionId = uuid.v4();
 
 const options = {
@@ -58,13 +59,45 @@ app.post('/chatbot', async (request, response) => {
     // }
     try {
         let botResult = await dialogClient.detectIntent(botRequest)
-        console.log('botresult',botResult)
+            .then(console.log('botresult before',botResult))
+            .then(botResult => {
+                if (botResult.queryResult.allRequiredParamsPresent) {
+                    matchIntent(botResult)
+                }
+            })
+            .then(console.log('botresult after', botResult))
         response.send(botResult[0])
     } catch(error) {
         console.log(error)
         response.send({message: 'blahblah'})
     }
 })
+
+const findTest = location => {
+    axios.get(`https://covid-19-testing.github.io/locations/${location.state}/complete.json`)
+        .then(response => response.json())
+        .then(result => {
+            const siteCheck = result.select(site => site.physical_address[0].city == location.city)
+            console.log('siteCheck', siteCheck)
+            if (siteCheck.physical_address) {
+                botResult.queryResult.fulfillmentText = `There's a test center at ${siteCheck.physical_address.address_1}.`
+            }
+        })
+}
+
+// const intents = {
+//     "Find Test Location": findTest
+// }
+
+const matchIntent = botResult => {
+    let middleIntent = botResult.queryResult.intent
+    let middleParams = botResult.queryResult.parameters
+    if (middleIntent.displayName == "Find Test Location") {
+        findTest(middleparams)
+    }
+    // const { intent, parameters } = botResult.queryResult
+    // intents[intent.displayName](parameters)
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, '0.0.0.0')
