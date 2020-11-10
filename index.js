ac3041f889440cce833a340ffdd50527b4388883
Ben Rowe <brow495@gmail.com>
2020-11-10 09:39:53 -0700

    Debug.

--- index.js
+++ index.js
@@ -58,9 +58,6 @@ app.post('/chatbot', async (request, response) => {
     // }
     try {
         let botResult = await dialogClient.detectIntent(botRequest)
-        console.log('botresult before parse',botResult)
-        botResult = JSON.parse(botResult[0])
-        console.log('botresult after parse',botResult)
 
         console.log('botresult parameters before match',botResult[0].queryResult.parameters)
         console.log('botresult intent before match',botResult[0].queryResult.intent)
